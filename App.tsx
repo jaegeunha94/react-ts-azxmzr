93929c19bb1cb2a3103275125d26a4de419467ea
jaegeunha94 <jaegeunha94@gmail.com>
2022-08-05 13:41:05 +0900

    Update React Lazy test code

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
 import './style.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ReactLazyMain from './src/Performance/ReactLazy/ReactLazyMain';
-import ReactLazy from './src/Performance/ReactLazy/ReactLazy';
-import ReactLazyTest from './src/Performance/ReactLazy/ReactLazyTest';
+
+const Home = React.lazy(() =>
+  import('./src/Performance/ReactLazy/ReactLazyTest')
+);
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ReactLazyTest />} />
-        <Route path="/main" element={<ReactLazyMain />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
